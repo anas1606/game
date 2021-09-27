122c7a4625508b01426c59bf043814a9a871109c
anas1606 <anas@eliteevince.in>
2021-09-27 12:08:22 +0530

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  login = false;
-  constructor() { }
+  login = true;
+  reactiveForm: FormGroup;
+  submitted:boolean = false;
+
+  constructor(private formbuilder: FormBuilder) {
+    this.reactiveForm = this.formbuilder.group({
+      username: new FormControl('', [Validators.required]),
+      firstname: new FormControl('', [Validators.required, Validators.pattern("^[^\\s]+")]),
+      lastname: new FormControl('', [Validators.required, Validators.pattern("^[^\\s]+")]),
+      password: new FormControl('', [Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=(.*[0-9]))[a-zA-Z0-9]{8,}$")]),
+      confirmpassword: new FormControl('', [Validators.required]),
+    },
+      {
+        validators: this.MustMatch('password', 'confirmpassword'),
+      },
+    );
+  }
+
+  get validate(){return this.reactiveForm.controls}
+
+  MustMatch(password: string , confirmpassword: string){
+    return (formGroup:FormGroup)=>{
+      const control = formGroup.controls[password];
+      const match = formGroup.controls[confirmpassword];
+      if(match.errors && !match.errors.MustMatch){
+        return
+      }
+      if(control.value !== match.value){
+        match.setErrors({MustMatch:true});
+      }else{
+        match.setErrors(null);
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.submitted = true;
+    alert(this.reactiveForm.valid);
+  }
 }
